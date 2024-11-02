37be5a3aa1658c3aa946daebb8c129dbf75abc49
ariel halili <arielhalili259@gmail.com>
2024-11-02 23:04:56 +0200

    upload final code

--- components/translator.js
+++ components/translator.js
@@ -3,8 +3,100 @@ const americanToBritishSpelling = require('./american-to-british-spelling.js');
 const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
+function swapKeyValue(json) {
+    return Object.fromEntries(Object.entries(json).map(([key, value]) => [value, key]));
+}
+
+function capitalizeFirstLetter(text) {
+    return text.charAt(0).toUpperCase() + text.substring(1);
+}
+
+const britishToAmericanSpelling = swapKeyValue(americanToBritishSpelling);
+const britishToAmericanTitles = swapKeyValue(americanToBritishTitles);
+
+
 class Translator {
 
+    britishToAmericanLocale() { return 'british-to-american'; }
+    americanToBritishLocale() { return 'american-to-british'; }
+
+    translateWord(word, locale) {
+        const titles = locale == this.americanToBritishLocale()
+            ? Object.keys(americanToBritishTitles)
+            : Object.values(americanToBritishTitles);
+
+        let hasDot = word.endsWith('.');
+        let isTitle = word.charAt(0) == word.charAt(0).toUpperCase();
+
+        if (hasDot) {
+            if (locale == this.americanToBritishLocale() && titles.includes(word.toLowerCase())) {
+                word = word.toLowerCase();
+                hasDot = false;
+            }
+            else {
+                word = word.slice(0, -1); // remove dot
+            }
+        }
+
+        if (isTitle && locale == this.britishToAmericanLocale()) {
+            if (titles.includes(word.toLowerCase()) ||
+                Object.keys(britishOnly).includes(word.toLowerCase()))
+                word = word.toLowerCase();
+        }
+
+        const timeFormat = locale === this.britishToAmericanLocale() ? /\b(\d{1,2})\.(\d{2})\b/g : /\b(\d{1,2}):(\d{2})\b/g;
+        let translatedWord = (locale === this.britishToAmericanLocale()
+            ? britishToAmericanSpelling[word] || britishOnly[word] || britishToAmericanTitles[word]
+            : americanToBritishSpelling[word] || americanOnly[word] || americanToBritishTitles[word]
+        ) || word.replace(timeFormat, locale === this.britishToAmericanLocale() ? '$1:$2' : '$1.$2');
+
+        if (isTitle)
+            translatedWord = capitalizeFirstLetter(translatedWord);
+
+        if (translatedWord != word)
+            translatedWord = '<span class=\"highlight\">' + translatedWord + '</span>';
+
+        if (hasDot)
+            translatedWord += '.'; // add dot back
+
+        return translatedWord;
+    }
+
+    // tranlate text. if it changed add `span` to highlight changes.
+    translate(text, locale) {
+        if (locale != this.americanToBritishLocale() && locale != this.britishToAmericanLocale())
+            throw new Error('Invalid value for locale field');
+
+        let words = text.split(/\s/);
+        let translatedWords = [];
+        for (let i = 0; i < words.length; i++) {
+            let word = words[i];
+
+            const phrasesDict = locale == this.americanToBritishLocale() ? americanOnly : britishOnly;
+
+            // find biggest phrase words' count
+            const wordsCount = Object.keys(phrasesDict).reduce((count, key) => {
+                return (key == word.toLowerCase() || key.startsWith(word.toLowerCase() + ' ')) && key.split(' ').length > count
+                    ? key.split(' ').length : count
+            }, 1);
+
+            let translatedWord = this.translateWord(word, locale);
+
+            if (wordsCount > 1) {
+                // try translate complete phrase
+                const phrase = words.slice(i, i + wordsCount).join(' ').toLowerCase();
+                const translatedPhrase = this.translateWord(phrase, locale);
+                if (translatedPhrase != phrase) {
+                    translatedWord = translatedPhrase; // apply change
+                    i += wordsCount - 1; // advance words' pointer
+                }
+            }
+
+            translatedWords.push(translatedWord);
+        }
+
+        return capitalizeFirstLetter(translatedWords.join(' '));
+    }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,24 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
+
   const translator = new Translator();
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      const { text, locale } = req.body;
+
+      if (text == undefined || locale == undefined)
+        return res.json({ error: 'Required field(s) missing' });
+      if (text.length == 0)
+        return res.json({ error: 'No text to translate' });
+      if (locale != translator.americanToBritishLocale() && locale != translator.britishToAmericanLocale())
+        return res.json({ error: 'Invalid value for locale field' });
+
+      let translation = translator.translate(text, locale);
+      if (translation == text)
+        translation = 'Everything looks good to me!';
+
+      res.json({ text, translation });
     });
 };
